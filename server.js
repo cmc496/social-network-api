6e128881447af7c94a8edeb32f22ac56165d249c
Agatha Harkness <caitlinmarie.clifford@gmail.com>
2022-08-06 19:12:26 -0400

    added line of code to index.js in api folder to properly access api routes

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api-api', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
 mongoose.set('debug', true);
 
-app.listen(PORT, () => console.log(`Connected to localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Connected to localhost:${PORT}`))
